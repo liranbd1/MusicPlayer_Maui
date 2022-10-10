5d41ed325603d1aec205f0d34cd0bfd29e7b5817
Liran <liranbd1@gmail.com>
2022-10-10 16:21:28 +0300

    Add ground work for Next and Prev inteructions

--- MyMusicPlayer/Models/MusicPlayer.cs
+++ MyMusicPlayer/Models/MusicPlayer.cs
@@ -11,28 +11,52 @@ namespace MyMusicPlayer.Models
         private double _currentSongPosition;
         private SongFile _currentSong;
 
+        private static bool _didSongEnd;
+        private static bool _didUserRequestNextSong;
+        private static bool _didUserRequestPrevSong;
+        private static bool _isPaused;
+
         public MusicPlayer(IAudioManager audioManager)
         {
+            _isPaused = false;
             _audioManager = audioManager;
             _currentSongIndex = 0;
         }
 
         public double Duration => _currentPlayer.Duration;
 
-        public void PlayPlaylist(Playlist playlist)
+        public async void PlayPlaylist(Playlist playlist)
         {
-            foreach(var song in playlist.SongsFile)
-            {
-                PlaySong(song);
-
-                while (_currentPlayer.IsPlaying)
+            _isPaused = false;
+            await Task.Run(() => { 
+                foreach(var song in playlist.SongsFile)
                 {
-                    if (_currentPlayer.CurrentPosition >= _currentPlayer.Duration)
+                    while (_isPaused) { }
+
+                    PlaySong(song);
+
+                    while (_currentPlayer.IsPlaying)
                     {
-                        break;
+                        if (_didSongEnd)
+                        {
+                            _didSongEnd = false;
+                            break;
+                        }
+
+                        if (_didUserRequestNextSong)
+                        {
+                            _didUserRequestNextSong = false; //True will come from a user button.
+                            break;
+                        }
+
+                        if (_didUserRequestPrevSong)
+                        {
+                            _didUserRequestPrevSong = false; //True will come from a user button.
+                            break;
+                        }
                     }
                 }
-            }
+            });
         }
 
         /// <summary>
@@ -41,26 +65,36 @@ namespace MyMusicPlayer.Models
         /// <param name="song"></param>
         public async void PlaySong(SongFile song)
         {
+            _isPaused = false;
             if (_currentPlayer != null)
             {
+                //This is the resume scenario
                 if (song.Equals(_currentSong))
                 {
                     _currentPlayer.Play();
                     _currentPlayer.Seek(_currentSongPosition);
                 }
+                //Playing a new song
                 else
                 {
                     _currentPlayer.Stop();
+                    _currentPlayer.PlaybackEnded -= _currentPlayer_PlaybackEnded;
                     _currentPlayer.Dispose();
+                    PlayNewSong(song);
                 }
             }
 
             else
             {
-                _currentSong = song;
-                _currentPlayer = _audioManager.CreatePlayer(_currentSong.Path);
-                _currentPlayer.Play();
+                PlayNewSong(song);
             }
+
+            _currentPlayer.PlaybackEnded += _currentPlayer_PlaybackEnded;
+        }
+
+        private void _currentPlayer_PlaybackEnded(object sender, EventArgs e)
+        {
+            _didSongEnd = true;
         }
 
         public void PauseSong()
@@ -72,6 +106,26 @@ namespace MyMusicPlayer.Models
 
             _currentSongPosition = _currentPlayer.CurrentPosition;
             _currentPlayer.Pause();
+            _currentPlayer.PlaybackEnded -= _currentPlayer_PlaybackEnded;
+            _isPaused = true;
+
+        }
+
+        public void NextSong()
+        {
+            _didUserRequestNextSong = true;
+        }
+
+        public void PrevSong()
+        {
+            _didUserRequestPrevSong = true;
+        }
+
+        private void PlayNewSong(SongFile song)
+        {
+            _currentSong = song;
+            _currentPlayer = _audioManager.CreatePlayer(_currentSong.Path);
+            _currentPlayer.Play();
         }
     }
 }
